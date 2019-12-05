e85989afce536b42655b40ff31d7bf4367cbe227
Maximilian Torggler <maximilian.torggler@tuta.io>
2019-12-05 09:40:45 +0100

    Better buffer vis.

--- src/loopstation/core/audio-looper/AudioLooperVisualization.ts
+++ src/loopstation/core/audio-looper/AudioLooperVisualization.ts
@@ -1,49 +1,103 @@
 import AudioLooper from './AudioLooper';
 
-function drawAudio(canvas: HTMLCanvasElement, buffer: AudioBuffer) {
-  const resolution = 50;
-  const rect = canvas.getBoundingClientRect();
-  canvas.width = Math.floor(rect.width * devicePixelRatio);
-  canvas.height = Math.floor(rect.height * devicePixelRatio);
-  const context = canvas.getContext('2d');
-  if (!context) return;
-  context.fillStyle = '#fff';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  let data = buffer.getChannelData(0);
-  context.fillStyle = '#000';
-  for (let i = 0; i < canvas.width; i++) {
-    let max = -Infinity;
-    let min = Infinity;
-    const start = Math.floor((i / canvas.width) * data.length);
-    const end = Math.floor(((i + 1) / canvas.width) * data.length);
-    const interval = Math.floor((end - start) / resolution) || 1;
-    for (let j = start; j <= end; j += interval) {
-      const item = data[j];
-      if (max < item) max = item;
-      if (min > item) min = item;
-    }
-    const height = ((max - min) / 2) * canvas.height;
-    const startPixel = (1 - (max + 1) / 2) * canvas.height;
-    context.fillRect(i, startPixel, 1, height);
-  }
+interface AudioBufferVisualizationData {
+  audioBuffers: AudioBuffer[];
+  percentualProgress: number;
 }
 
-function drawTimePointer(
-  canvas: HTMLCanvasElement,
-  percentualProgress: number,
-) {
-  const context = canvas.getContext('2d');
-  if (!context) return;
-  const xPos = canvas.width * percentualProgress;
-  context.fillStyle = 'red';
-  context.fillRect(xPos, 0, 2, canvas.height);
+/**
+ * Visualize multiple audio buffers
+ */
+class AudioBufferVisualization {
+  private availableColors: string[] = ['#000', 'blue', 'red'];
+  private canvas!: HTMLCanvasElement;
+  private visualizationData: AudioBufferVisualizationData;
+
+  constructor() {
+    this.visualizationData = {
+      audioBuffers: [],
+      percentualProgress: 0,
+    };
+    this.setup();
+  }
+
+  private setup() {
+    const CANVAS_HEIGHT = 200;
+    const canvasId = `bufferCanvas`;
+    const containerHtml = `
+        <div style="position: absolute; right: 0; top: 0px; width: 40%; height: ${CANVAS_HEIGHT}px;">
+          <canvas style="width: 100%; height: 100%" id="${canvasId}" />
+        </div>
+      `;
+    document.body.insertAdjacentHTML('beforeend', containerHtml);
+    const canvasEl = document.querySelector<HTMLCanvasElement>(`#${canvasId}`);
+    this.canvas = canvasEl as HTMLCanvasElement;
+    const context = this.canvas.getContext('2d');
+    if (!context) return;
+
+    const rect = this.canvas.getBoundingClientRect();
+    this.canvas.width = Math.floor(rect.width * devicePixelRatio);
+    this.canvas.height = Math.floor(rect.height * devicePixelRatio);
+  }
+
+  private drawAudioBuffers() {
+    const resolution = 50;
+
+    const context = this.canvas.getContext('2d');
+    if (!context) return;
+
+    this.visualizationData.audioBuffers.forEach((buffer, i) => {
+      let data = buffer.getChannelData(0);
+      context.fillStyle = this.availableColors[i];
+      for (let i = 0; i < this.canvas.width; i++) {
+        let max = -Infinity;
+        let min = Infinity;
+        const start = Math.floor((i / this.canvas.width) * data.length);
+        const end = Math.floor(((i + 1) / this.canvas.width) * data.length);
+        const interval = Math.floor((end - start) / resolution) || 1;
+        for (let j = start; j <= end; j += interval) {
+          const item = data[j];
+          if (max < item) max = item;
+          if (min > item) min = item;
+        }
+        const height = ((max - min) / 2) * this.canvas.height;
+        const startPixel = (1 - (max + 1) / 2) * this.canvas.height;
+        context.fillRect(i, startPixel, 1, height);
+      }
+    });
+  }
+
+  private drawTimePointer() {
+    const context = this.canvas.getContext('2d');
+    if (!context) return;
+    const xPos = this.canvas.width * this.visualizationData.percentualProgress;
+    context.fillStyle = 'red';
+    context.fillRect(xPos, 0, 2, this.canvas.height);
+  }
+
+  private draw() {
+    const context = this.canvas.getContext('2d');
+    if (!context) return;
+
+    context.fillStyle = '#fff';
+    context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.drawAudioBuffers();
+    this.drawTimePointer();
+  }
+
+  public update(data: AudioBufferVisualizationData) {
+    this.visualizationData = data;
+    this.draw();
+  }
 }
 
 export default class AudioLooperVisualization {
   private audioLooper: AudioLooper;
+  private audioBufferVisualization: AudioBufferVisualization;
 
   constructor(audioLooper: AudioLooper) {
     this.audioLooper = audioLooper;
+    this.audioBufferVisualization = new AudioBufferVisualization();
   }
 
   public start() {
@@ -56,26 +110,14 @@ export default class AudioLooperVisualization {
       measureDuration,
       currentMeasureOffset,
     } = this.audioLooper.getVisualizationData();
-    audioBuffers.forEach((audioBuffer, i) => {
-      if (audioBuffer.buffer) {
-        const CANVAS_HEIGHT = 200;
-        const canvasId = `bufferCanvas_${i}`;
-        const containerHtml = `
-        <div style="position: absolute; right: 0; top: ${CANVAS_HEIGHT *
-          i}px; width: 40%; height: ${CANVAS_HEIGHT}px;">
-          <canvas style="width: 100%; height: 100%" id="${canvasId}" />
-        </div>
-      `;
-        document.body.insertAdjacentHTML('beforeend', containerHtml);
-        const canvasEl = document.querySelector<HTMLCanvasElement>(
-          `#${canvasId}`,
-        );
-        if (!canvasEl) return;
-        drawAudio(canvasEl, audioBuffer.buffer);
-        const percentualProgress = currentMeasureOffset / measureDuration;
-        drawTimePointer(canvasEl, percentualProgress);
-      }
+
+    this.audioBufferVisualization.update({
+      audioBuffers: audioBuffers
+        .map(buffer => buffer.buffer)
+        .filter(buffer => buffer) as AudioBuffer[],
+      percentualProgress: currentMeasureOffset / measureDuration,
     });
+
     window.requestAnimationFrame(this.draw.bind(this));
   }
 }
